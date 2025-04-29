87fe5e6da1a1bd169fc95a5ced55e91383504be6
Damon Edstrom <damon@edstrom.io>
2025-04-29 16:10:17 -0500

    Fixed to reflect the lack of directory state in lua interpreter

--- scratch/todo/startUp.lua
+++ scratch/todo/startUp.lua
@@ -1,8 +1,6 @@
-shell.run("cd todo")
--- Read urls.json and get the code url
-
+local baseDir = "./todo"
 print("Reading urls.json...")
-local urls_raw = fs.open("urls.json", "r")
+local urls_raw = fs.open(fs.combine(baseDir, "urls.json"), "r")
 local urls = textutils.unserializeJSON(urls_raw.readAll())
 urls_raw.close()
 
@@ -14,11 +12,12 @@ local code = code_raw.readAll()
 code_raw.close()
 
 print("Writing code to todo_display.lua...")
-local code_file = fs.open("todo_display.lua", "w")
+local code_file = fs.open(fs.combine(baseDir, "todo_display.lua"), "w")
 code_file.write(code)
 code_file.close()
 
 print("Running todo display...")
 -- Change the file for todo in urls.json to specific todo list if not generic
 -- TODO: Add support for multiple types of todo rather than this hack
+shell.run("cd todo")
 shell.run("todo_display")
